07eccd415b0f18c5a94f728c97960733f9d6c7ab
Jose Mayor <josemayor@yahoo.es>
2025-11-14 11:28:26 +0100

    start withmain generic class-based views

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -1,7 +1,11 @@
 # load a view when the user visits the root URL
 from django.urls import path
 from . import views
+from .views import DesignPageView, RunPageView, DebugPageView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
+    path("design/", DesignPageView.as_view(), name="design"),
+    path("run/", RunPageView.as_view(), name="run"),
+    path("debug/", DebugPageView.as_view(), name="debug"),
 ]
--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    context = {
+        "greeting": "Thank you for visiting.",
+        "options": [
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+        ],
+    }
+    return render(request, "pages/home.html", context)
 
+
+class DesignPageView(TemplateView):
+    template_name = "pages/design-page.html"
+
+
+class RunPageView(TemplateView):
+    template_name = "pages/run-page.html"
+
+
+class DebugPageView(TemplateView):
+    template_name = "pages/debug-page.html"
