bbfcea5403740a95e898c122915e625f963b475d
Jose Mayor <josemayor@yahoo.es>
2025-12-16 22:42:10 +0100

    chore: remove activate.bat and users app cleanup

--- activate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo -----------------------------------------------------------------------------------------------
-echo Activando entorno virtual de Python
-call venv\Scripts\activate.bat
-@echo Entorno virtual activado. Escribe 'deactivate' para salir
-cd mysite
-python manage.py runserver
-@cmd /k
